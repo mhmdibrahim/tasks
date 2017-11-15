31387dc28a9339a19be299bdf8332d542e78f3b2
Mhmd Ibrahim <mhmdibrahimabdellatif@gmail.com>
2017-11-15 11:54:59 +0200

    change

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -17,6 +17,12 @@ class TasksController extends Controller
         $request->validate([
             'task' => 'required|max:255'
         ]);
+//        $task = Task::with([
+//            'user.task'=>function($query){
+//            $query->where('created_at','>=',Carbon::today())->
+//                where('created_at','<',Carbon::tomorrow());
+//            }
+//        ])->find($user);
         $task = new Task();
         $task->content = $request->task;
         $task->user_id =auth()->user()->id;
