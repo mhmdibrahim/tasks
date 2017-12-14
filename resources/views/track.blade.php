a1ec99dca21d18e37227651d987587cb2f70e7d8
Amany Ahmed <amany000hassan@gmail.com>
2017-12-14 11:40:43 +0200

    tracker Departments changes

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -13,6 +13,131 @@
         </div>
             <div class="container">
                 @forelse($departments as $department)
+                <div class="row">
+                <div class="col-lg-4">
+                    <div class="ibox">
+                        <div class="ibox-title">
+                           
+                            <h3 class="left">{{$department->name}} </h3><br/>
+                        </div>
+                        
+                           
+                        <div class="ibox-content auto-height">
+<!--
+                            <div class="emp-title">
+                                 <h5 class="pull-left">Department Employees</h5>
+                             <span class=" pull-right">View All Employess </span>
+                            </div>
+-->
+                           
+                            <div class="feed-activity-list">
+                                 <ol>
+                                    <div class="feed-element">
+                                        <div class="media-body ">
+                                            @forelse($department->employees as $employee)
+                                            <ul>
+                                            <strong>{{$employee->first_name}} {{$employee->last_name}}</strong><br>
+                                            <li>
+                                                <a href="profile.html" class="pull-left">
+                                                    <img alt="" class="img-circle" src="/images/Blank_Avatar.png" >
+                                                </a>
+                                            </li>
+                                            <small class="text-muted">{{$employee->job_title}}</small>-
+                                            <small class="text-muted">{{$employee->phone}}</small>
+                                            </ul>
+                                            @empty @lang('No Employees')
+                                            @endforelse
+                                        </div>
+                                     </div>
+                                    </ol>
+                                </div>
+
+                                <div class="feed-element">
+                                    <a href="profile.html" class="pull-left">
+                                        <img alt="" class="img-circle" src="/images/Blank_Avatar.png" >
+                                    </a>
+                                    <div class="media-body ">
+                                        <strong>Employee Name</strong><br>
+                                        <small class="text-muted">Job Title</small>
+                                        <small class="text-muted">Phone Number</small>
+                                    </div>
+                                </div>
+
+                                <div class="feed-element">
+                                    <a href="profile.html" class="pull-left">
+                                        <img alt="" class="img-circle" src="/images/Blank_Avatar.png" >
+                                    </a>
+                                    <div class="media-body ">
+                                        <strong>Employee Name</strong><br>
+                                        <small class="text-muted">Job Title</small>
+                                        <small class="text-muted">Phone Number</small>
+                                    </div>
+                                </div>
+                                <div class="feed-element">
+                                    <a href="profile.html" class="pull-left">
+                                        <img alt="" class="img-circle" src="/images/Blank_Avatar.png" >
+                                    </a>
+                                    <div class="media-body ">
+                                        <strong>Employee Name</strong><br>
+                                        <small class="text-muted">Job Title</small>
+                                        <small class="text-muted">Phone Number</small>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="row  card-footer">
+                                <div class="col-sm-6">
+                                    <div class="font-bold">TASKS NUMBER</div>
+                                    12
+                                </div>
+                                
+                                <div class="col-sm-6 text-right ">
+                                    <button class="btn btn-primary btn-block m-t"> All Tasks</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                  
+                   
+                
+             
+                   
+                </div>
+            </div>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                     <div class="notification  col-md-6">
                     <h3 class="notification-title"> {{$department->name}}</h3>
                     <ol class="custom-counter1 auto-height">
