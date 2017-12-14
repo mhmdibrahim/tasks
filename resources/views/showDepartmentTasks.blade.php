042c255fcdbe27105945b5fe586efbedba570b06
Amany Ahmed <amany000hassan@gmail.com>
2017-12-14 18:03:28 +0200

    edit department tasks list & department employees tasks

--- resources/views/showDepartmentTasks.blade.php
+++ resources/views/showDepartmentTasks.blade.php
@@ -6,10 +6,8 @@
 @endsection
 @section('content')
     <main class="container @if($tasks <1) default @endif">
-        
         <!--        employee details     -->
-        
-        <section class="row department ">
+        <section class="row department">
             <h2 class="heading">@lang('Department Tasks')</h2>
             <div>
                 <ul class="breadcrumb">
@@ -19,45 +17,41 @@
             </div>
             <section class="addSec">
                 <form id="form" method="GET">
-                    <div class="form-group">
-                                <div class="controls DateControl">
-                                    <label for="arrive" class="label-date">&nbsp;&nbsp;@lang('Choose date')</label>
+                    <div class="form-group date-form">
+                        <div class="controls DateControl">
+                            <label for="arrive" class="label-date">&nbsp;&nbsp;@lang('Choose date')</label>
 <!--                                <input type="date" id="arrive" class="floatLabel" name="date" value="{{$date}}">-->
-                                    <input class="form-control" type="date" id="arrive" class="floatLabel" name="date" value="{{$date}}">
-                                </div>
+                            <input class="form-control" type="date" id="arrive" class="floatLabel" name="date" value="{{$date}}">
+                        </div>
                     </div>
                 </form>
             </section>
-            
-            
-
-            <div class="container">
-                <div class="notification  col-md-12">
-                    <h3 class="notification-title">{{$department->name}}</h3>
-                    <ol class="custom-counter auto-height">
-                        @foreach($department->employees as $employee)
-                            @foreach($employee->tasks as $task)
-                                <li>{{$task->content}}</li>
-                            @endforeach
-                        @endforeach
-                        @if($tasks<1)
-                                <div class="icon text-center">
-                                    <div class="block">
-                                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                                    </div>
-                                    <p>@lang('No Tasks')</p>
-                                </div>
-                        @endif
-                    </ol>
-                    
+            <div class="tasks-box col-md-12">
+                <div class="dep-title">
+                    <h3 class="left">{{$department->name}} </h3><br/>
                 </div>
+                <ol class="tasks-list auto-height">
+                    @foreach($department->employees as $employee)
+                        @foreach($employee->tasks as $task)
+                            <li class="task">{{$task->content}}</li>
+                        @endforeach
+                    @endforeach
+                    @if($tasks<1)
+                            <div class="icon text-center">
+                                <div class="block">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </div>
+                                <p>@lang('No Tasks')</p>
+                            </div>
+                    @endif
+                </ol>
             </div>
-
+            <div class="footer">
+                <a href="/moreDetails/{{$department->id}}" class="btn btn-primary">@lang('View Employees Tasks')</a>
+            </div>
         </section>
     </main>
-    <div class="footer">
-        <a href="/moreDetails/{{$department->id}}" class="btn btn-default">@lang('Details')</a>
-    </div>
+    
 @endsection
 @section('scripts')
     <script>
